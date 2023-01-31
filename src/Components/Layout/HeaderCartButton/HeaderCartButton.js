dc677ac2a1747c4d53d2f710bd1bb5c4cbce2df5
simon2385 <simon23vasquez@gmail.com>
2023-01-31 19:40:54 +0100

    Individual meals items

--- src/Components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -6,11 +6,11 @@ import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = () => {
   return (
     <button className={classes.button}>
-      <spam className={classes.icon}>
+      <div className={classes.icon}>
         <CartIcon />
-      </spam>
-      <spam>Your Cart</spam>
-      <spam className={classes.badge}>3</spam>
+      </div>
+      <div>Your Cart</div>
+      <div className={classes.badge}>3</div>
     </button>
   );
 };
